ee88c270cd9714809ecc352ce9b88b610e02a262
Michael Ball <michaelballqa@gmail.com>
2025-04-21 13:13:23 +0100

    test

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,25 +1,49 @@
 describe('checkout item', ()=>{
     const shoppingItem = "Sauce Labs Bike Light"
-    it('checkout', ()=>{
+    it('adds a product then checks it out', function(){
+        //login
         cy.visit('/');
-        cy.fixture('example').then((user) => {
-            cy.get('#user-name').type(user.username);
-            cy.get('#password').type(user.password);
-          });          
-        //cy.get('#user-name').type('standard_user');
+        cy.login();
         
-        //cy.get('#password').type('secret_sauce');
-        cy.get('#login-button').click();
+        // products page
         cy.url().should('contain', 'inventory');
-        cy.get('.shopping_cart_link').find('.shopping_cart_badge').should('not.exist');    
-        cy.get(`.inventory_item_description:contains(${shoppingItem})`).as('itemBlock');
+        cy.get('.shopping_cart_link').find('.shopping_cart_badge').should('not.exist');
+        cy.get(`.inventory_item_description:contains(${shoppingItem})`).as('itemBlock').should('exist');;
+        cy.get('@itemBlock').find('.inventory_item_price').invoke('text').then((price) => {
+            cy.wrap(price).as('itemPrice'); 
+          });
+        
+        // add product to cart
         cy.get('@itemBlock').find('button').as('itemBlockButton').click();
-        cy.get('@itemBlockButton').contains('Remove');
-        cy.get('.shopping_cart_link').find('.shopping_cart_badge');
+        cy.get('@itemBlockButton').should('contain', 'Remove');
+        cy.get('.shopping_cart_link').find('.shopping_cart_badge').should('be.visible');
         
+        // go to cart
+        cy.get('.shopping_cart_link').click();
+        cy.get('.cart_quantity').should('contain', '1');
+        cy.get('@itemPrice').then(()=>{
+            cy.get('.inventory_item_price').should('have.text', this.itemPrice);
+        })
         
-        
-    })
-    
+        // checkout step 1
+        cy.get('#checkout').click();
+        cy.url().should('contain', 'checkout-step-one');
+        cy.get('#continue').click();
+        cy.get('.error-button').should('be.visible');
+        cy.get('#first-name').type('Michael');
+        cy.get('#last-name').type('Ball');
+        cy.get('#postal-code').type('PE27 6UF');
+        cy.get('#continue').click();
 
+        // checkout step 2
+        cy.url().should('contain', 'checkout-step-two');
+        cy.get('#finish').click();
+
+        // checkout complete
+        cy.url().should('contain', 'checkout-complete');
+        cy.get('.pony_express').should('be.visible');
+        cy.get('.complete-header').should('be.visible');
+        cy.get('.complete-text').should('be.visible');
+        cy.get('#back-to-products').click();
+    })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', () => {
+    cy.fixture('users').then((user) => {
+            cy.get('#user-name').type(user.username);
+            cy.get('#password').type(user.password);
+          });
+        cy.get('#login-button').click();
+  });
+  
